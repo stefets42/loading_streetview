067cfdbad8f70de483a27d20c86132e366a69657
stefets42 <stefets42@users.noreply.github.com>
2017-10-10 18:23:32 +0200

    Correct typo

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function loadData() {
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+'<a href="' + article.web_url + '">' + article.headline.main + '</a>'+'<p>'+article.snippet+'</p>'+'</li>');
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
         };
     })
 
