2abe24c40e5fb0594d4c9f90b25ea3e2df67c983
stefets42 <stefets42@users.noreply.github.com>
2017-10-10 17:32:51 +0200

    Use jQuery AJAX requests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# loading_streetview
+Enter an address on this webpage. You will see:
+### a picture of it;
+### articles about it.
+
+It is an exercise to learn how to use jQuery AJAX requests with the Google Streetview API and the New York Times API.
--- js/script.js
+++ js/script.js
@@ -10,9 +10,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE! 
+    // Google Streetview AJAX request to load streetview image
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
@@ -22,6 +20,18 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // NYTimes AJAX request to load NYTimes information
+    var nytimeUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + 'sort=newest&api-key=12bf6b4ce1a14f938f87f65f49fe5bdd'
+    $.getJSON(nytimeUrl, function(data){
+        $nyHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>'+article.snippet+'</p>'+'</li>');
+        };
+    })
 
     return false;
 };
