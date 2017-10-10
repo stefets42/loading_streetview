a0c2f847d9d2a3ffd10cd547ca78be3f3290cb1d
stefets42 <stefets42@users.noreply.github.com>
2017-10-10 18:59:45 +0200

    Add a Wikipedia AJAX request

--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ function loadData() {
 
     // NYTimes AJAX request to load NYTimes information
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=12bf6b4ce1a14f938f87f65f49fe5bdd'
-    $.getJSON(nytimesUrl, function(data) {
+    $.getJSON(nytimesUrl, function(data){
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
         };
-    }).fail(function(e) {
+    }).fail(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
@@ -41,7 +41,7 @@ function loadData() {
         url: wikiUrl,
         dataType: "jsonp",
         // jsonp: "callback",
-        success: function(response) {
+        success: function(response){
             var articleList = response[1];
 
             for (var i = 0; i < articleList.length; i++) {
