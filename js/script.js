facc5f7568bb5a7b6ff5affbc67d36981631780e
stefets42 <stefets42@users.noreply.github.com>
2017-10-10 17:40:36 +0200

    Correct typo

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
-<<<<<<< HEAD
 
     // NYTimes AJAX request to load NYTimes information
     var nytimeUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + 'sort=newest&api-key=12bf6b4ce1a14f938f87f65f49fe5bdd'
