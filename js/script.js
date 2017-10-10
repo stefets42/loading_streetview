98d22888cfb09a6c7ebe5099d0dae31954e19310
stefets42 <stefets42@users.noreply.github.com>
2017-10-10 18:39:46 +0200

    Add a Wikipedia AJAX request

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // NYTimes AJAX request to load NYTimes information
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + 'sort=newest&api-key=12bf6b4ce1a14f938f87f65f49fe5bdd'
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=12bf6b4ce1a14f938f87f65f49fe5bdd'
     $.getJSON(nytimesUrl, function(data){
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
@@ -33,6 +33,24 @@ function loadData() {
         };
     }).fail(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    // Wikipedia AJAX request to load NYTimes information
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        // jsonp: "callback",
+        success: function(response){
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+        }
+
     });
 
     return false;
